05dc7375509765dc5b2382fa888fcad7da86eb33
slj99 <17590579+slj99@users.noreply.github.com>
2023-08-27 17:55:03 +0800

    Create sql2csv.py

---
+++ sql2csv.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+import csv
+import re
+
+table_name = ''
+headers = []
+data = []
+
+
+def write_csv(table_name):
+  # write_csv
+    with open(f'{table_name}.csv', 'w', newline='', encoding='utf-8') as f:
+        writer = csv.writer(f)
+        writer.writerow(headers)
+        writer.writerows(data)
+
+
+with open('20230827-114512-1-v1.4.6.sql', 'r', encoding='utf-8') as f:
+    lines = f.readlines()
+
+    for line in lines:
+        if line.startswith('CREATE TABLE'):
+            write_csv(table_name)
+            headers = []
+            data = []
+            # table_name
+            table_name = re.search(r'`(.*?)`', line).group(1)
+        elif line.startswith('INSERT INTO'):
+            # headers
+            headers = re.findall(r'`(.*?)`', line)
+            # data
+            values = re.search(r'\((.*)\)', line).group(1)
+            row = values.split(',')
+            data.append(row)
